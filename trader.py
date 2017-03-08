1be15ef4dcdedf9ca2fdae5532c166c30f4790e9
priyank96 <priyankbhandia@hotmail.com>
2017-03-08 18:04:04 +0530

    Update trader.py

--- trader.py
+++ trader.py
@@ -1,71 +1,47 @@
-""" Simulates trading, logs trading action in tradeLog.p"""
-
-# from __future__ import division
-from time import sleep
+from headline_to_symbol import headline_to_symbol
 from datetime import datetime, time
+import csv
+from closest_lines import closest_lines
+from EntityClass import *
+from plotter import plot, prepare_plots
+import numpy as np
+from time import sleep
 
-import pickle
-import headline_to_symbol as hs
+entities = headline_to_symbol()
+prepare_plots(entities)
+num_entities = len(entities)
+print(num_entities)
+clock = datetime.now()
+minutes = 0
 
+while clock.time() < time(15, 30):
+    clock = datetime.now()
+    if clock.time() > time(9, 14):
+        while datetime.now().second != 59:  # start at end of minute
+            continue
+        minutes += 1
+        for i in range(0, num_entities):
+            entities[i].update_values()
+            with open(entities[i].file_name, 'r') as csvfile:
+                line = []
+                start = False
+                spamreader = csv.reader(csvfile, delimiter=',')
+                for row in spamreader:
 
+                    if not start and row:
+                        start = float(row[1])
+                        line.append(0.0)
+                    elif row:
+                        # line.append(float(i + 1))
+                        line.append((float(row[1]) - start) * 100 / start)
 
-def tradeWith(entity):
-    entity.updateValues()
-    if entity.holdState == 1:
-        return 1  # done trading, running for price history
-    if entity.cornerPoint < 0 and entity.holdState == 0 and entity.prevDayDelta[-1] > 0:  # Sell
-        entity.holdState = -1
-        entity.trade.append(entity.priceList[-1])
-        print(entity.trade[0])
+            similar_lines = list(map(lambda x: x[1], closest_lines(line, minutes)))
+            while len(line) != len(similar_lines[0]):
+                 line.append(np.nan)
+            similar_lines.append(line)
+            # print('here')
+            plot(similar_lines, entities[i].id)
 
-    if entity.holdState == -1 and ((entity.trade[0] - entity.priceList[-1]) / entity.priceList[-1]) > 0.0039:  # Buy
-        entity.holdState = 1
-        entity.trade.append(entity.priceList[-1])
-        print((entity.trade[0] - entity.priceList[-1]) / entity.priceList[-1])
-
-
-    return -1  # not done trading, don't get rid of entity
-
-
-if __name__ == "__main__":
-
-    entities = hs.headline_to_symbol()  # stocks to trade on the Bombay Stock Exchange
-
-    while datetime.now().second != 59:  # start at end of minute
-        continue
-
-    tradeLog = {entity.ID: [] for entity in entities}
-
-    while datetime.now().time() < time(15, 30):
-        if datetime.now().time() > time(9, 14):
-            for entity in entities:
-                if tradeWith(entity) == 1:  # done trading
-
-                    tradeLog[entity.ID] = entity.trade
-                    entity.log()
-                    #entities.remove(entity)
-                    continue
-
-                else:
-
-                    tradeLog[entity.ID] = entity.trade
-                    entity.log()
-
-        else:
-            print("waiting " + str(datetime.now().time()))
-
-        with open('TradeLog.p', 'wb') as fil:
-            pickle.dump(tradeLog, fil)
-        sleep(60)
-        # if i.holdState == 1 and i.cornerPoint>0:
-        #    i.holdState = 2
-        #    i.trade.append(i.priceList[-1])
-        #    print (i.trade[1])
-        #    tradeLog[i.ID].append(i.priceList[-1])
-
-
-        # if i.holdState == 2  and i.cornerPoint<0 and ((i.priceList[-1]-i.trade[1])/i.trade[1])>0.0030 :
-        #    i.holdState = 5
-        #    tradeLog[i.ID].append(i.priceList[-1])
-
-    print("Done- trader")
+    else:
+        print("waiting " + str(clock.time()))
+    sleep(60)
