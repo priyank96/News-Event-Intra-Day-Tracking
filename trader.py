28e74a90701d58e2ba88c387c7d7a5278a933e18
Priyank Bhandia <priyankbhandia@gmail.com>
2017-05-03 22:37:37 +0530

    Update trader.py

--- trader.py
+++ trader.py
@@ -8,6 +8,7 @@ from time import sleep
 entities = headline_to_symbol()
 prepare_plots(entities)
 num_entities = len(entities)
+
 minutes = 0
 clock = datetime.now()
 
@@ -15,6 +16,7 @@ while clock.time() < time(15, 30):
     clock = datetime.now()
     if clock.time() > time(9, 14):
         minutes += 1
+
         while datetime.now().second != 59:  # start at end of minute
             continue
 
@@ -22,8 +24,7 @@ while clock.time() < time(15, 30):
             entity.update_values()
             similar_lines = list(map(lambda x: x[1], closest_lines(entity.line, minutes)))
             similar_lines.append(entity.line)
-            # print('here')
-            plot(entity.line, entity.id)
+            plot(similar_lines, entity.id)
 
     else:
         print("waiting " + str(clock.time()))
