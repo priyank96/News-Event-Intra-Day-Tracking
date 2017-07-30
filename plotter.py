3a70fe4f4a29e0016fd3eacb24e63d63ab553c58
Priyank Bhandia <priyankbhandia@gmail.com>
2017-07-30 16:27:04 +0530

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,17 +1,23 @@
-from bokeh.charts import output_file, Line, save
+from bokeh.plotting import output_file, save, figure
 from selenium import webdriver
 from EntityClass import EntityClass
 plots = dict()
 
 def prepare_plots(entities):
+    done = []
     for i in entities:
-        plots[i.id] = webdriver.Chrome()
-
+        if i.id not in done:
+            plots[i.id] = webdriver.Chrome()
+        done.append(i.id)
 
 def plot(lines, id):
     #print(lines)
     output_file(id+".html",title=id)
-    p = Line(lines,plot_width=1000,plot_height=800)
+    p = figure(plot_width=600,plot_height=600)
+    x_axis_values = []
+    for line in lines:
+        x_axis_values.append(list(range(0, len(line)-1)))
+    p.multi_line(xs = x_axis_values, ys = lines, color = ['blue','green','purple'])
     save(p)
     plots[id].get("file:///C:/Users/Prism/Desktop/Priyank/csp5/"+id+".html")
     #plots[id].navigate().to("file:///C:/Users/Prism/Desktop/Priyank/csp5/"+id+".html")
@@ -20,7 +26,7 @@ def plot(lines, id):
 
 if __name__=='__main__':
     from closest_lines import get_lines_hl
-    lines = get_lines_hl()
+    lines = get_lines_hl(60)
     prepare_plots([EntityClass('Sample')])
     plot(lines,'Sample')
     plot(lines, 'Sample')
