37f018fd9844d0d36212aad13beab03b16e7d119
Priyank Bhandia <priyankbhandia@gmail.com>
2017-08-11 21:49:28 +0530

    Update data_logger.py

--- data_logger.py
+++ data_logger.py
@@ -5,51 +5,30 @@ import pandas as pd
 import os.path
 import datetime
 
+str_of_date = str(datetime.date.today())
+
+
 def log_data(entities):
-    today = str(datetime.date.today())
     file_name = EntityClass.past_trades_file_name
     if os.path.isfile(file_name):
-        try:
-            past_trades_hl = pd.read_hdf(file_name, key='headline')
-            fresh_trades_hl = pd.DataFrame()
-            for entity in entities:
-                fresh_trades_hl[entity.headline] = entity.line
-            past_trades_hl = pd.concat([past_trades_hl, fresh_trades_hl], axis=1)
-        except KeyError:
-            past_trades_hl = pd.DataFrame()
-            for entity in entities:
-                past_trades_hl[entity.headline] = entity.line
-        try:
-            past_trades_date = pd.read_hdf(file_name, key='date')
-            fresh_trades_date = pd.DataFrame()
-            for entity in entities:
-                fresh_trades_date[entity.headline] = entity.line
-            past_trades_date = pd.concat([past_trades_date, fresh_trades_date], axis=1)
-        except KeyError:
-            past_trades_date = pd.DataFrame()
-
-            for x, entity in enumerate(entities):
-                past_trades_hl[today + " " + str(x)] = entity.line  # unique column headings are ensured
+        past_trades_date = pd.read_hdf(file_name, key='date')
+        fresh_trades_date = pd.DataFrame()
+        for entity in entities:
+            fresh_trades_date[entity.headline + ' ' + str_of_date] = entity.line
+        past_trades_date = pd.concat([past_trades_date, fresh_trades_date], axis=1)
 
     hdf = pd.HDFStore(file_name)
-    hdf.put('headline', past_trades_hl, data_columns=True)
+
     hdf.put('date', past_trades_date, data_columns=True)
     print(past_trades_date.shape)
 
-if __name__ == '__main__':
-    try:
-        past_trades = pd.read_hdf('PastTrades.h5', key='headline')
-        print(past_trades.columns)
-        print(past_trades)
 
-    except KeyError as e:
-        print(e)
+if __name__ == '__main__':
+
     try:
         past_trades = pd.read_hdf('PastTrades.h5', key='date')
-        print(list(past_trades))
-        print(past_trades)
+        print(past_trades.columns.values)
+
 
     except KeyError as e:
         print(e)
-
-
