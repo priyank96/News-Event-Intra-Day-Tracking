365c868ac32bf1f8553025d2061a2323a9bc97de
Priyank Bhandia <priyankbhandia@gmail.com>
2017-10-09 16:04:03 +0000

    Update trader.py

--- trader.py
+++ trader.py
@@ -5,33 +5,36 @@ from plotter import plot, prepare_plots
 from data_logger import log_data
 from time import sleep
 from EntityClass import EntityClass
-import numpy as np
 import pandas as pd
 
 
 # readying entities
 entities = headline_to_symbol()
+for i in entities:
+    print(i.id+' '+i.headline)
 ans = str(input("Add entity? (y/n)"))
 while ans == 'y':
     symbol = str(input("Enter symbol:"))
     headline = str(input("Enter headline:"))
     entities.append(EntityClass(symbol, headline))
     ans = str(input("Add entity? (y/n)"))
-prepare_plots(entities)
+#prepare_plots(entities)
 num_entities = len(entities)
 
 # readying stored data
 data_fil = 'PastTrades.h5'
 data = pd.read_hdf(data_fil, key='date')  # data frame of past prices
+print(data.columns.values)
 format_lines(data)
 print("Done- data load")
 
 minutes = -1
 clock = datetime.now()
 
-while clock.time() < time(15, 30):
+while  clock.time() < time(15, 30):
     clock = datetime.now()
     if clock.time() > time(9, 14):
+        print(minutes)
         minutes += 1
 
         while datetime.now().second != 59:  # start at end of minute
@@ -40,14 +43,16 @@ while clock.time() < time(15, 30):
         for entity in entities:
             entity.update_values()
             try:
-                list_closest_lines = get_closest_line(entity.line)
-                list_closest_lines.append(entity.line)
-                plot(list_closest_lines, entity.id)
+                #list_closest_lines = get_closest_line(entity.line)
+                #list_closest_lines.append(entity.line)
 
-            except:
-                pass
+                plot([entity.line], entity.id)
+
+            except Exception as e:
+                print(repr(e))
 
     else:
         print("waiting " + str(clock.time()))
-    sleep(60)
+        sleep(40)
+
 log_data(entities)
