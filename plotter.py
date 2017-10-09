39d35a94c13df346f00d85598fed694e4554a612
Priyank Bhandia <priyankbhandia@gmail.com>
2017-10-09 16:04:26 +0000

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,25 +1,22 @@
 from bokeh.plotting import output_file, save, figure
 from selenium import webdriver
-from EntityClass import EntityClass
+
 plots = dict()
 
 def prepare_plots(entities):
     done = []
     for i in entities:
         if i.id not in done:
-            plots[i.id] = webdriver.Chrome()
+            plots[i.id] = webdriver.Firefox()
         done.append(i.id)
 
 def plot(lines, id):
     #print(lines)
     output_file(id+".html",title=id)
     p = figure(plot_width=600,plot_height=600)
-    x_axis_values = []
-    for line in lines:
-        x_axis_values.append(list(range(0, len(line)-1)))
-    p.multi_line(xs = x_axis_values, ys = lines, color = ['blue','green','purple'])
+    p.line(x=list(range(0,len(lines[0]))),y=lines[0])
     save(p)
-    plots[id].get("file:///C:/Users/Prism/Desktop/Priyank/csp5/"+id+".html")
+    #plots[id].get("file:///home/priyank/Documents/News-Event-Intra-Day-Tracking-master/"+id+".html")
     #plots[id].navigate().to("file:///C:/Users/Prism/Desktop/Priyank/csp5/"+id+".html")
 
 
